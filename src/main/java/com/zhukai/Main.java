30c7cd18d9d433b943101a803112b8579409abb3
zhukai <657263524@qq.com>
2018-01-07 20:40:25 +0800

    Thymeleaf test

--- src/main/java/com/zhukai/Main.java
+++ src/main/java/com/zhukai/Main.java
@@ -4,30 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class Main implements EmbeddedServletContainerCustomizer {
+@Controller
+public class Main {
 	
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	
 	@RequestMapping("/")
-	public String getHello() {
+	public String getHello(Model model) {
 		log.info("getHello in....");
-		return "Hello Spring Boot .....";
-	}
-
-	// 修改访问的默认端口
-	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
-		configurableEmbeddedServletContainer.setPort(80);
+		model.addAttribute("hello", "Thymeleaf");
+		return "index";
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
-		log.info("SpringBootApplication Service start.");
+		log.info("SpringBootApplication Service start...");
 	}
 }
