ca79ff2b03d168e6dc8feb3bc5da65eee90d3370
zhukai <657263524@qq.com>
2018-06-13 22:35:44 +0800

    生成代码

---
+++ generate-code/src/main/java/com/zhukai/entity/ColumnEntity.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zhukai.entity;
+
+/**
+ * 列的属性
+ */
+public class ColumnEntity {
+	
+	// 列名
+	private String columnName;
+	// 列名类型
+	private String dataType;
+	// 列名备注
+	private String comments;
+	// 属性名称(第一个字母大写)，如：user_name => UserName
+	private String attrName;
+	// 属性名称(第一个字母小写)，如：user_name => userName
+	private String attrname;
+	// 属性类型
+	private String attrType;
+	// auto_increment
+	private String extra;
+
+	public String getAttrname() {
+		return attrname;
+	}
+
+	public void setAttrname(String attrname) {
+		this.attrname = attrname;
+	}
+
+	public String getAttrName() {
+		return attrName;
+	}
+
+	public void setAttrName(String attrName) {
+		this.attrName = attrName;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+
+	public String getDataType() {
+		return dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getAttrType() {
+		return attrType;
+	}
+
+	public void setAttrType(String attrType) {
+		this.attrType = attrType;
+	}
+
+	public String getExtra() {
+		return extra;
+	}
+
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+}
